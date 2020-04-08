8b27d0be5dbf1e19a9d5bc624b1a4376d002844f
kausthub kodamagulla <47001638+Dude-1999@users.noreply.github.com>
2020-04-08 16:20:59 +0530

    Add files via upload

---
+++ Load_data.R
@@ -0,0 +1,14 @@
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile=paste0(getwd(),"/Power_consumption.zip"),method = "curl")
+unzip("Power_consumption.zip",exdir="./")
+
+hpc<-read.table("household_power_consumption.txt", header=TRUE,sep=";", skip=66630, 
+                nrows=2900, col.names=names(initial), na.strings=c("?"),
+                colClasses=c("character", "character","numeric","numeric","numeric","numeric",
+                             "numeric","numeric","numeric"))
+
+hpc$Date<-as.Date(hpc$Date, format = "%d/%m/%Y")
+hpc$Time<-strptime(paste(hpc$Date,hpc$Time),"%F %T")
+
+hpc<-subset(hpc,hpc$Date %in% as.Date(c("2007-02-01","2007-02-02")))
+
